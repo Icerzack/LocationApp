c3f04dad23fa143a334c0911cc0d40b6d036263c
Icerzack <maxalkuz@gmail.com>
2022-06-28 16:19:44 +0300

    add app base

---
+++ LocationApp/LocationApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationApp
+//
+//  Created by Max Kuznetsov on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
